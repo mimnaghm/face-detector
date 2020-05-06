39f6ee5853777fac034bd98b459b6e0390886854
Marcus Mimnagh <marcusmimnagh@Marcuss-MacBook-Pro.local>
2020-05-06 18:03:59 -0500

    Updates

---
+++ precache-manifest.635fc5c548bec5fe64b67a5e030fecca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf67487ab43e04addfae50a92ca821d",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "cbd0c9d8b2447deaedda",
+    "url": "/face-detector/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "5ac03498f56c1cbdc780",
+    "url": "/face-detector/static/css/main.57412864.chunk.css"
+  },
+  {
+    "revision": "cbd0c9d8b2447deaedda",
+    "url": "/face-detector/static/js/2.3920e286.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detector/static/js/2.3920e286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac03498f56c1cbdc780",
+    "url": "/face-detector/static/js/main.0ebb8683.chunk.js"
+  },
+  {
+    "revision": "065033ff78af409e336b",
+    "url": "/face-detector/static/js/runtime-main.b8a88ace.js"
+  },
+  {
+    "revision": "71dd7432d7b15adcc5feeaeb64c05ab7",
+    "url": "/face-detector/static/media/Azonix.71dd7432.woff"
+  },
+  {
+    "revision": "727139a7ab86109b9a654882a3b3982c",
+    "url": "/face-detector/static/media/face.727139a7.png"
+  }
+]);
